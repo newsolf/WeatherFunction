6522670f302b03fbc21ad20942f26964496ecef4
NeWolf <newolf006@126.com>
2021-06-25 19:02:25 +0800

    add BlackNineAdapter

--- app/src/main/java/com/newolf/weatherfunction/black/BlackActivity.kt
+++ app/src/main/java/com/newolf/weatherfunction/black/BlackActivity.kt
@@ -1,18 +1,33 @@
 package com.newolf.weatherfunction.black
 
 
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.newolf.weatherfunction.R
 import com.newolf.weatherfunction.app.base.BaseActivity
+import java.util.*
 
 
 class BlackActivity : BaseActivity() {
-
+    private lateinit var blackNineAdapter: BlackNineAdapter
     override fun bindLayout(): Int {
         return R.layout.activity_black
     }
 
     override fun initView() {
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        rvList.layoutManager = GridLayoutManager(mContext, 3)
+        blackNineAdapter = BlackNineAdapter(this, null)
+        rvList.adapter = blackNineAdapter
+    }
 
+    override fun loadData() {
+        val data = ArrayList<String>()
+
+        for (index in 1..9){
+            data.add("$index")
+        }
+        blackNineAdapter.setNewInstance(data)
     }
 
     override fun initListener() {
--- app/src/main/java/com/newolf/weatherfunction/black/BlackNineAdapter.kt
+++ app/src/main/java/com/newolf/weatherfunction/black/BlackNineAdapter.kt
@@ -1,43 +1,32 @@
-package com.newolf.weatherfunction.welcome
+package com.newolf.weatherfunction.black
 
-import android.text.TextUtils
 import androidx.lifecycle.LifecycleOwner
 import com.ewolf.wolfanim.RotateImageView
 import com.newolf.library.adapt.base.BaseQuickAdapter
 import com.newolf.library.adapt.base.viewholder.BaseViewHolder
 import com.newolf.weatherfunction.R
-import com.newolf.weatherfunction.app.Navigate
-import okhttp3.internal.toHexString
 
 /**
- * NineAdapter
+ * BlackNineAdapter
  *
  * @author NeWolf
  * @since 2021-06-25
  */
-class NineAdapter(private val lifecycleOwner: LifecycleOwner, data: MutableList<String>?) : BaseQuickAdapter<String, BaseViewHolder>(R.layout.adapter_nine,data) {
-    private val stringBuilder  = StringBuilder()
+class BlackNineAdapter(private val lifecycleOwner: LifecycleOwner, data: MutableList<String>?) : BaseQuickAdapter<String, BaseViewHolder>(R.layout.adapter_black_nine,data) {
 
 
     override fun convert(holder: BaseViewHolder, item: String) {
         val rotateImageView =
             holder.getView<RotateImageView>(R.id.riv_show).addLifecycleObserver(lifecycleOwner)
-        rotateImageView.stop()
         holder.itemView.setOnClickListener {
             if (rotateImageView.isAnimStart()){
                 rotateImageView.stop()
             }else{
                 rotateImageView.start()
             }
-            stringBuilder.append(item)
-            if (TextUtils.equals(stringBuilder.toString().toInt().toHexString(),"1850e7")){
-                Navigate.startBlackActivity(context)
-            }
         }
 
-        when (item) {
-            "1", "3", "7", "9" -> rotateImageView.start()
-        }
+
 
     }
 
